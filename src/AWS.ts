03eb48551a445cf212cb1df1b960fa0f28371dd5
vincecoppola <vincecoppola@gmail.com>
2018-02-17 18:50:40 -0800

    Also use AWS env variables if set

--- src/AWS.ts
+++ src/AWS.ts
@@ -2,12 +2,20 @@ import AWS from 'aws-sdk';
 
 import { DEFAULTS } from './constants';
 
+const {
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_DEFAULT_REGION,
+} = process.env;
+
 /**
- * By default, load credentials from the Shared Credentials File
+ * By default, load credentials from environment variables or the Shared Credentials File.
  * {@link https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html as recommended}.
  */
-const defaultCredentials = new AWS.SharedIniFileCredentials();
+const defaultCredentials: AWS.Credentials = AWS_ACCESS_KEY_ID
+  ? new AWS.Credentials(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+  : new AWS.SharedIniFileCredentials();
 AWS.config.credentials = defaultCredentials;
-AWS.config.region = DEFAULTS.REGION;
+AWS.config.region = AWS_DEFAULT_REGION || DEFAULTS.REGION;
 
 export default AWS;
